05503550cbc73eb839c5a1f7445989455abd2268
david-amos <57191710+david-amos@users.noreply.github.com>
2019-11-06 12:21:44 -0600

    Story 2+3

---
+++ CodeDrawMcGraw/CodeDrawMcGraw/Story2.vb
@@ -0,0 +1,10 @@
+﻿Public Class Story2
+    Private Sub Cont_Click(sender As Object, e As EventArgs) Handles Cont.Click
+        Dim frmStory3 As New Story3
+        Story3.ShowDialog()
+    End Sub
+
+    Private Sub Story2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.SelectionStart = 0
+    End Sub
+End Class
---
+++ CodeDrawMcGraw/CodeDrawMcGraw/Story3.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Story3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Cont = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.InfoText
+        Me.TextBox1.Font = New System.Drawing.Font("Castellar", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.Window
+        Me.TextBox1.Location = New System.Drawing.Point(61, 247)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(517, 164)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "*The Doctor Patches you up*  Wow you killed big hex! He's the meanest Gang leader" &
+    " around. His gang will come for you for sure!"
+        '
+        'Cont
+        '
+        Me.Cont.Font = New System.Drawing.Font("Castellar", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Cont.Location = New System.Drawing.Point(593, 319)
+        Me.Cont.Name = "Cont"
+        Me.Cont.Size = New System.Drawing.Size(183, 49)
+        Me.Cont.TabIndex = 2
+        Me.Cont.Text = "Continue"
+        Me.Cont.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.PictureBox1.Location = New System.Drawing.Point(187, 39)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(201, 202)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Story3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Cont)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Story3"
+        Me.Text = "Code-Draw McGraw"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Cont As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
