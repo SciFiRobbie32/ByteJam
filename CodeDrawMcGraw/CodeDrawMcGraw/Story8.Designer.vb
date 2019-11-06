b8eadf603f83cdcbb9a9cac3c4a869f4d4a0d3a2
david-amos <57191710+david-amos@users.noreply.github.com>
2019-11-06 13:45:13 -0600

    Story7+8

--- CodeDrawMcGraw/CodeDrawMcGraw/Story8.Designer.vb
+++ CodeDrawMcGraw/CodeDrawMcGraw/Story8.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Story6
+Partial Class Story8
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -23,7 +23,9 @@ Partial Class Story6
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Cont = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -31,37 +33,49 @@ Partial Class Story6
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Castellar", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLight
-        Me.Label1.Location = New System.Drawing.Point(193, 203)
+        Me.Label1.Location = New System.Drawing.Point(20, 280)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(414, 33)
+        Me.Label1.Size = New System.Drawing.Size(760, 33)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "I must save the doctor!"
+        Me.Label1.Text = "Now its just us, you wont get me that easy!"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.DarkRed
+        Me.PictureBox1.Location = New System.Drawing.Point(285, 82)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(190, 158)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
         '
         'Cont
         '
         Me.Cont.Font = New System.Drawing.Font("Castellar", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Cont.Location = New System.Drawing.Point(309, 297)
+        Me.Cont.Location = New System.Drawing.Point(309, 364)
         Me.Cont.Name = "Cont"
         Me.Cont.Size = New System.Drawing.Size(183, 49)
-        Me.Cont.TabIndex = 4
+        Me.Cont.TabIndex = 6
         Me.Cont.Text = "Continue"
         Me.Cont.UseVisualStyleBackColor = True
         '
-        'Story6
+        'Story8
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.Cont)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label1)
-        Me.Name = "Story6"
+        Me.Name = "Story8"
         Me.Text = "Code-Draw McGraw"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Cont As Button
 End Class
--- CodeDrawMcGraw/CodeDrawMcGraw/Story8.vb
+++ CodeDrawMcGraw/CodeDrawMcGraw/Story8.vb
@@ -1,4 +1,4 @@
-﻿Public Class Story6
+﻿Public Class Story8
     Private Sub Cont_Click(sender As Object, e As EventArgs) Handles Cont.Click
         Me.Close()
     End Sub
