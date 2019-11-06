85be0f18df2568f6fb055cee1bef8dc7b5604c8c
Robbie <robbie_hannah@stu.indianhills.edu>
2019-11-06 09:53:42 -0600

    MainMenu

--- CodeDrawMcGraw/CodeDrawMcGraw/main.vb
+++ CodeDrawMcGraw/CodeDrawMcGraw/main.vb
@@ -1,3 +1,17 @@
 ﻿Public Class main
+    Private Sub btnNewGame_Click(sender As Object, e As EventArgs) Handles btnNewGame.Click
 
+    End Sub
+
+    Private Sub btnCredits_Click(sender As Object, e As EventArgs) Handles btnCredits.Click
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
